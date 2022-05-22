dd902b7a32d5a1a0461f0beb5b31fd16523800ac
toufiqul-hasan <toufiqul.shihab@gmail.com>
2022-05-22 13:19:58 +0600

    business summary added

---
+++ src/Pages/Home/BusinessSummary.js
@@ -0,0 +1,87 @@
+import React from "react";
+
+const BusinessSummary = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Business Summary</h1>
+      <div className="lg:grid grid-cols-4">
+        <div className="card py-5">
+          <div className="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-10 w-10"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+            </svg>
+          </div>
+          <div className="stat-title">Total Customers</div>
+          <div className="stat-value text-secondary">20K+</div>
+          <div className="stat-desc">30% more than last year</div>
+        </div>
+        <div className="card py-5">
+          <div className="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-10 w-10"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="stat-title">Annual Revenue</div>
+          <div className="stat-value text-secondary">10M+</div>
+          <div className="stat-desc">20% more than last year</div>
+        </div>
+        <div className="card py-5">
+          <div className="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-10 w-10"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="stat-title">Total Reviews</div>
+          <div className="stat-value text-secondary">1K+</div>
+          <div className="stat-desc">25% more than last year</div>
+        </div>
+        <div className="card py-5">
+          <div className="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-10 w-10"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              />
+              <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
+            </svg>
+          </div>
+          <div className="stat-title">Total Tools</div>
+          <div className="stat-value text-secondary">90+</div>
+          <div className="stat-desc">5% more than last year</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BusinessSummary;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import Banner from './Banner';
+import React from "react";
+import Banner from "./Banner";
+import BusinessSummary from "./BusinessSummary";
 
 const Home = () => {
   return (
-    <div className='py-16'>
-      <Banner/>
+    <div className="py-16">
+      <div>
+        <Banner />
+      </div>
+      <div className="py-10 text-center">
+        <BusinessSummary />
+      </div>
     </div>
   );
 };
