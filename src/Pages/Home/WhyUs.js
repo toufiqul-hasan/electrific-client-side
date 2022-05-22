6dc266ae1a250fbb8c979c19f0334c35410b81ec
toufiqul-hasan <toufiqul.shihab@gmail.com>
2022-05-22 19:08:22 +0600

    auth added

--- src/Pages/Home/WhyUs.js
+++ src/Pages/Home/WhyUs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import chart from "../../Assets/Images/chart.png";
 
 const WhyUs = () => {
@@ -8,26 +9,27 @@ const WhyUs = () => {
       <div className="hero mt-5">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img src={chart} alt="" />
-          <div>
+          <div className="text-justify">
             <h1 className="text-xl font-bold mt-10">
-              We are leading company in this field, We provide specific
+              We are leading company in this field. We provide specific
               solutions for our every customers.
             </h1>
             <p>
-              When you give to Our Charity, know your donation is making a
-              difference Whether you are supporting one our Signature Programs
-              or our carefully curated list of Gifts That Give More, our
-              professional staff. Manufactory partner with over 320 amazing
-              projects worldwide, and have given over $150 million product.
+              When you give to our charity, know your donation is making a
+              difference Whether you are supporting one our signature programs
+              or our carefully curated list of gifts that give more, our
+              professional staff. Electrific partner with over 320 amazing
+              projects worldwide and have given over $10 million product.
             </p>
             <p>
-              We partner with over 320 amazing ut projects worldwide, and have
-              given over $150 million in cash and product grants to other
-              groups.
+              We partner with over 320 amazing tech projects worldwide and have
+              given over $15 million in cash and product grants to other groups.
             </p>
-            <button className="btn btn-primary mt-5 text-white">
-              Learn More
-            </button>
+            <Link to="/blog">
+              <button className="btn btn-primary mt-5 text-white">
+                Learn More
+              </button>
+            </Link>
           </div>
         </div>
       </div>
